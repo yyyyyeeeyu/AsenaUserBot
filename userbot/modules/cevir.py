904fdb39198b301887dad604b397ab64d2d28174
yyyyyyeeee <80161799+yyyyyeeeyu@users.noreply.github.com>
2022-06-04 18:08:13 +0400

    Update cevir.py

--- userbot/modules/cevir.py
+++ userbot/modules/cevir.py
@@ -1,10 +1,10 @@
-# Copyright (C) 2020 Yusuf Usta.
+# Copyright (C) 2022 Bloodper.
 #
 # Licensed under the  GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
+# Lexa UserBot - Bloodper
 
 from userbot import CMD_HELP
 from userbot.events import register
@@ -45,7 +45,7 @@ async def cevir(event):
 
         im = Image.open(foto).convert("RGB")
         im.save("sticker.png", "png")
-        await event.client.send_file(event.chat_id, "sticker.png", reply_to=rep_msg, caption="@AsenaUserBot `ile fotoğrafa çevirildi.`")
+        await event.client.send_file(event.chat_id, "sticker.png", reply_to=rep_msg, caption="@LexaUserBot `ile fotoğrafa çevirildi.`")
 
         await event.delete()
         os.remove("sticker.png")
@@ -70,7 +70,7 @@ async def cevir(event):
             indir = await rep_msg.download_media()
             ses = await asyncio.create_subprocess_shell(f"ffmpeg -i '{indir}' {KOMUT[efekt]} output.mp3")
             await ses.communicate()
-            await event.client.send_file(event.chat_id, "output.mp3", reply_to=rep_msg, caption="@AsenaUserBot `ile efekt uygulandı.`")
+            await event.client.send_file(event.chat_id, "output.mp3", reply_to=rep_msg, caption="@LexaUserBot `ile efekt uygulandı.`")
             
             await event.delete()
             os.remove(indir)
@@ -96,7 +96,7 @@ async def cevir(event):
         await event.edit(f"`{LANG['UPLOADING_GIF']}`")
 
         try:
-            await event.client.send_file(event.chat_id, "out.gif",reply_to=rep_msg, caption=LANG['WITH_ASENA_GIF'])
+            await event.client.send_file(event.chat_id, "out.gif",reply_to=rep_msg, caption=LANG['WITH_LEXA_GIF'])
         except:
             await event.edit(LANG['ERROR'])
             await event.delete()
@@ -120,7 +120,7 @@ async def cevir(event):
         await event.edit('`Ses yükleniyor...`')
         
         try:
-            await event.client.send_file(event.chat_id, "out.mp3",reply_to=rep_msg, caption='@AsenaUserBot ile sese çevrildi.')
+            await event.client.send_file(event.chat_id, "out.mp3",reply_to=rep_msg, caption='@LexaUserBot ile sese çevrildi.')
         except:
             os.remove(video)
             return await event.edit('`Sese çevirilemedi!`')

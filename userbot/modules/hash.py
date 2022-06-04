ab2bdc9dcaeee56c6655533ee99a6e47f0ba8b7e
yyyyyyeeee <80161799+yyyyyeeeyu@users.noreply.github.com>
2022-06-04 21:04:59 +0400

    Update hash.py

--- userbot/modules/hash.py
+++ userbot/modules/hash.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
+# Lexa UserBot - Bloodper
 
 
 """ Hash ve encode/decode çözme komutlarını içeren UserBot modülü. """

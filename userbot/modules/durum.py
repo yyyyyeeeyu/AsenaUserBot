a1cfc364e98ce96ad398c7d6d69d6c31c5ccb640
yyyyyyeeee <80161799+yyyyyeeeyu@users.noreply.github.com>
2022-06-04 19:33:33 +0400

    Update durum.py

--- userbot/modules/durum.py
+++ userbot/modules/durum.py
@@ -1,12 +1,12 @@
-# Copyright (C) 2020 Yusuf Usta.
+# Copyright (C) 2022 Bloodper.
 #
 # Licensed under the GPL-3.0 License;
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
+# Lexa UserBot - Bloodper
 
-from userbot import CMD_HELP, ASYNC_POOL, tgbot, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, LYDIA_API_KEY, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, ASENA_VERSION
+from userbot import CMD_HELP, ASYNC_POOL, tgbot, SPOTIFY_DC, G_DRIVE_CLIENT_ID, lastfm, LYDIA_API_KEY, YOUTUBE_API_KEY, OPEN_WEATHER_MAP_APPID, AUTO_PP, REM_BG_API_KEY, OCR_SPACE_API_KEY, PM_AUTO_BAN, BOTLOG_CHATID, LEXA_VERSION
 from userbot.events import register
 from telethon import version
 from platform import python_version
@@ -34,7 +34,7 @@ async def durums(event):
     await event.edit(f"""
 **Python {LANG['VERSION']}:** `{python_version()}`
 **TeleThon {LANG['VERSION']}:** `{version.__version__}` 
-**Asena {LANG['VERSION']}:** `{ASENA_VERSION}`
+**Lexa {LANG['VERSION']}:** `{LEXA_VERSION}`
 
 **{LANG['PLUGIN_COUNT']}:** `{len(CMD_HELP)}`
 

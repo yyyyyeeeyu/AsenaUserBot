6c5fb5e36c5ea5cbe1aaae8be56884fe86817094
yyyyyyeeee <80161799+yyyyyeeeyu@users.noreply.github.com>
2022-06-04 17:10:24 +0400

    Update and rename __asena.py to __lexa.py

--- userbot/modules/__lexa.py
+++ userbot/modules/__lexa.py
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 #
 
-# Asena UserBot - Yusuf Usta
+# Lexa Userbot - Bloodper
 #
 
 """ UserBot yardım komutu """
@@ -17,13 +17,13 @@ from userbot.events import register
 # ██████ LANGUAGE CONSTANTS ██████ #
 
 from userbot.language import get_value
-LANG = get_value("__asena")
+LANG = get_value("__lexa")
 
 # ████████████████████████████████ #
 
-@register(outgoing=True, pattern="^.asena(?: |$)(.*)")
-async def asena(event):
-    """ .asena komutu için """
+@register(outgoing=True, pattern="^.lexa(?: |$)(.*)")
+async def lexa(event):
+    """ .lexa komutu için """
     args = event.pattern_match.group(1).lower()
     if args:
         if args in CMD_HELP:
